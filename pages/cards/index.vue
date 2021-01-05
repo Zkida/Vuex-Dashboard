0a4f7a01691d088910048ab6cfb5419d066af79c
Joseph Ruano <seph.developer@gmail.com>
2021-01-04 22:59:33 -0600

    authenticated user on cards page

--- pages/cards/index.vue
+++ pages/cards/index.vue
@@ -1,3 +1,17 @@
 <template>
-  <div>Cards</div>
+  <div v-if="authenticated">
+    <div>Cards</div>
+  </div>
+  <div v-else>login</div>
 </template>
+<script>
+import { mapGetters } from 'vuex'
+export default {
+  computed: {
+    ...mapGetters({
+      authenticated: 'auth/authenticated',
+      user: 'auth/user',
+    }),
+  },
+}
+</script>
