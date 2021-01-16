f7674aa6fdd060a39047bd802c31db414ac36699
Joseph Ruano <seph.developer@gmail.com>
2021-01-15 20:41:21 -0600

    UI Changes

--- components/AppCard.vue
+++ components/AppCard.vue
@@ -1,29 +1,34 @@
 <template>
   <div class="qr-card mb-base">
-    <vs-card>
-      <template #title>
-        <h3>{{ card.name }}</h3>
-      </template>
-      <template #img>
-        <img src="https://vuesax.com/foto5.png" alt="" />
-      </template>
-      <template #text>
-        <p>{{ card.summary }}</p>
-      </template>
-      <template #interactions>
+    <div class="qr-data">
+      <div class="meta">
+        <div class="qr-name">
+          <i class="bx bx-credit-card-front"></i>
+          <h3>{{ card.name }}</h3>
+        </div>
+        <div class="qr-metadata">
+          <div class="summary">
+            <i class="bx bx-folder-open"></i>
+            <p class="text">{{ card.summary }}</p>
+          </div>
+          <div class="qr-date">
+            <i class="bx bx-time"></i>
+            <p>Date</p>
+          </div>
+        </div>
+      </div>
+      <div class="qr-link">
         <nuxt-link
           class="no-line"
           :to="{ name: 'dashboard-cards-slug', params: { slug: card.uuid } }"
         >
-          <vs-button warn gradient icon>
-            <i class="bx bxs-edit"></i>
-          </vs-button>
-        </nuxt-link>
-        <vs-button shadow icon>
           <i class="bx bx-link-external"></i>
-        </vs-button>
-      </template>
-    </vs-card>
+          Vista previa
+        </nuxt-link>
+      </div>
+    </div>
+    <div class="qr-code"></div>
+    <div class="qr-actions">BUTTON</div>
   </div>
 </template>
 
--- pages/dashboard/index.vue
+++ pages/dashboard/index.vue
@@ -1,13 +1,49 @@
 <template>
   <div>
-    <div>awesome dashboar hone</div>
+    <div v-if="$fetchState.pending" class="flex text-gray">
+      Cargando datos...
+    </div>
+    <p v-else-if="$fetchState.error">An error occurred :(</p>
+    <div v-else>
+      <template v-if="cards.length">
+        <div class="grid">
+          <vs-row class="grid-row">
+            <vs-col
+              v-for="card in cards"
+              :key="card.uuid"
+              vs-type="flex"
+              vs-justify="center"
+              vs-align="center"
+              w="12"
+            >
+              <app-card :card="card" />
+            </vs-col>
+          </vs-row>
+        </div>
+      </template>
+    </div>
   </div>
 </template>
 <script>
+import { mapActions, mapGetters } from 'vuex'
 export default {
   layout: 'main',
-  //   middleware({ redirect }) {
-  //     return redirect('/cards/dashboard')
-  //   },
+  middleware: 'auth',
+  computed: {
+    ...mapGetters({
+      cards: 'cards/cards',
+    }),
+  },
+  methods: {
+    ...mapActions({
+      getCardsAction: 'cards/getCards',
+    }),
+  },
+  async fetch() {
+    await this.getCardsAction()
+  },
+  mounted() {
+    this.$fetch()
+  },
 }
 </script>
